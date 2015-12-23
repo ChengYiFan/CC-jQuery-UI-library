b6e4734a6d72a368fdf3d7be718bd3df2a4e4331
Cynthia <chenyifan-2000@163.com>
2015-12-23 16:24:21 +0800

    update

--- review.js
+++ review.js
@@ -5,9 +5,6 @@ function showView(href){
 	newMask.id = "mask";
     setMask();
     document.body.appendChild(newMask);
-    window.onscroll=function(){
-  		document.body.scrollTop = 0;
-  	};
   	window.onresize = function(){
   		setMask();
   		popupCenter();
@@ -16,21 +13,21 @@ function showView(href){
     //弹出层
     var newDiv = document.createElement("div");
     newDiv.id = "popup";
-    newDiv.style.top = "60px";
+    newDiv.style.top = "80px";
     newDiv.style.left = "40px";
     popupCenter();
     var newiframe = document.createElement("iframe");
     newiframe.src = href;
     newDiv.appendChild(newiframe);
     $(newDiv).fadeIn(600, function() {
-    	
+    	document.body.style.overflow = "hidden";
     });
     document.body.appendChild(newDiv);
 
     //弹出层居中
     function popupCenter(){
-    	var width = document.body.clientWidth - 2*document.body.scrollLeft -80;
-    	var height = document.body.clientHeight - 2*document.body.scrollTop - 120;
+    	var width = document.body.clientWidth -80;
+    	var height = document.body.clientHeight - 360;
     	newDiv.style.width = width +"px";
     	newDiv.style.height = height+ "px";
     }
@@ -58,6 +55,8 @@ function showView(href){
     function closePopup(){
 		document.body.removeChild(docEle("mask"));
     	document.body.removeChild(docEle("popup"));
+        document.body.style.overflow = "";
+
 	}
 }
 // function initPopup(obj){
@@ -69,7 +68,6 @@ function showView(href){
 // });
 $(function(){
 	$(".demo").click(function(event){
-
 		var href = $(this).attr("href");
 		showView(href);
 		return false;
